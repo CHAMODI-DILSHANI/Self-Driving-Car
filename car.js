4cdf592d6f1196348f7f3245170e2b1152f24727
cham dil <cham.dil333@outlook.com>
2022-05-16 14:59:43 +0530

    making the car movements realistic according to physics

--- car.js
+++ car.js
@@ -5,32 +5,71 @@ class Car{
         this.w=w;
         this.h=h;
 
+        //setting up speed and acceleration
+        this.speed = 0;
+        this.acceleration =0.2;
+        this.maxSpeed = 2;
+        this.friction=0.05;
+        this.angle = 0;
+
+        //control the car movements
         this.controls = new Controls;
     }
 
     moving(){
         if(this.controls.forward){
-            this.y-=2;
+            this.speed+=this.acceleration;
         }
 
         if(this.controls.reverse){
-            this.y+=2;
+            this.speed-=this.acceleration;
         }
 
+        //to control the speed to not to go beyong maximum speed
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+
+        //to apply brakes
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+
+        //to stop car moving automatically without pressing any key according to friction
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        //this.y-=this.speed;
+
         if(this.controls.left){
-            this.x-=2;
+            this.angle+=0.03;
         }
 
         if(this.controls.right){
-            this.x+=2;
+            this.angle-=0.03;
         }
+
+        //making the vehicle move according to the turned direction
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
 
+    //draw rectangle to represent a car
     draw(ctx){
+        //making the vehicle rotate to left and right
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.w/2,
-            this.y-this.h/2,
+            -this.w/2,
+            -this.h/2,
             this.w,
             this.h
         );
--- main.js
+++ main.js
@@ -5,11 +5,18 @@ const context =canvas.getContext("2d");
 const car = new Car(100,100,30,50);
 car.draw(context);
 
+//animate the motion of car
 animate();
 
 function animate(){
 car.moving();
+
+//removing the previous postion of car and fitting the canvas to the window
 canvas.height=window.innerHeight;
+
+//drawing car according to the new coordinates
 car.draw(context);
+
+//drawing animation frame
 requestAnimationFrame(animate);
 }
